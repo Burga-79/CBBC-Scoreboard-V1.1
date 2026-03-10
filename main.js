a65b27794f82c5ce4d87ac436606451a899ad3a6
Burga-79 <adamburgess_79@bigpond.com>
2026-03-10 18:06:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,11 +8,9 @@ let serverProcess = null;
 
 /* -------------------------------------------------------
    START EXPRESS SERVER
-   - Dev: use require("./server.js")
-   - Packaged: spawn server.js via process.resourcesPath
 ------------------------------------------------------- */
 function startServerPackaged() {
-  if (serverProcess) return; // prevent multiple starts
+  if (serverProcess) return;
 
   const serverPath = path.join(process.resourcesPath || __dirname, "server.js");
 
@@ -34,7 +32,6 @@ function createWindows() {
   const external = displays.find((d) => d.id !== primary.id);
   const targetDisplay = external || primary;
 
-  // ADMIN WINDOW
   adminWindow = new BrowserWindow({
     width: 1400,
     height: 900,
@@ -46,7 +43,6 @@ function createWindows() {
 
   adminWindow.loadFile(path.join(__dirname, "admin", "admin.html"));
 
-  // DISPLAY WINDOW
   displayWindow = new BrowserWindow({
     x: targetDisplay.bounds.x,
     y: targetDisplay.bounds.y,
@@ -68,10 +64,8 @@ function createWindows() {
 ------------------------------------------------------- */
 app.whenReady().then(() => {
   if (app.isPackaged) {
-    // EXE mode: start server via spawned process
     startServerPackaged();
   } else {
-    // Dev mode: run server directly
     require("./server.js");
   }
 
